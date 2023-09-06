9169f649051894121d58dfeffbb55e2da1de7574
Han Chan <kong_du@hotmail.com>
2023-09-06 15:14:44 +0800

    add basic

---
+++ reviews.py
@@ -0,0 +1,11 @@
+data = []
+count = 0
+with open ('reviews.txt','r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0: # % = 求余數
+            print(len(data))
+print(len(data))
+print(data[0]) #印出第0筆資料
+
