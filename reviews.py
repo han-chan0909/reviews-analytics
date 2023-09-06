cb4d666447c6c77e1de18532bd2251845aac2034
Han Chan <kong_du@hotmail.com>
2023-09-06 15:23:47 +0800

    second vision

--- reviews.py
+++ reviews.py
@@ -7,5 +7,13 @@ with open ('reviews.txt','r') as f:
         if count % 1000 == 0: # % = 求余數
             print(len(data))
 print(len(data))
-print(data[0]) #印出第0筆資料
+print('檔案讀取完了,總共有', len(data), '筆資料') #印出第0筆資料
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print ('留言的平均長度', sum_len/len(data))
+
+
 
